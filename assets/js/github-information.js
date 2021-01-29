7173aab939835b75aa7bd06b093458c12eef1c7c
tizianocoppoli <tiziano.coppoli@gmail.com>
2021-01-29 15:30:35 +0000

    fix bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,9 @@ function repoInformationHTML(repos) {
 
 function fetchGitHubInformation(event) {
 
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     let username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`
@@ -74,3 +77,5 @@ function fetchGitHubInformation(event) {
         }
     );
 }
+
+$(document).ready(fetchGitHubInformation);
